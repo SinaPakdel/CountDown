04a21c06c59ec25ce7c183536158a58a11b1ecb8
Sina <sina.tabriziyan@gmail.com>
2023-09-07 13:53:48 +0330

    [feature] add CountDownViewModel

--- feature/countdown/src/main/java/ir/sina/countdown/CountDownViewModel.kt
+++ feature/countdown/src/main/java/ir/sina/countdown/CountDownViewModel.kt
@@ -1,4 +1,32 @@
 package ir.sina.countdown
 
-class CountDownViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class CountDownViewModel : ViewModel() {
+
+
+    private val _countdownState = mutableStateOf(CountdownState())
+    val countdownState: State<CountdownState> = _countdownState
 }
+
+
+data class CountdownState(
+    val counterState: CounterState = CounterState.INITIAL,
+    val workingState: WorkingState = WorkingState.REST,
+    val remainTime: Int = REST_DURATION,
+
+    )
+
+enum class CounterState {
+    INITIAL, PLAY, PAUSE
+}
+
+enum class WorkingState {
+    REST, WORK
+}
+
+const val REST_DURATION = 30
+const val WORKING_DURATION = 20 * 60
